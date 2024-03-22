bf93c9150d9829eaf1d6786fa40d0dd8b46e0cb8
Checho <ezequielayala.c@gmail.com>
2024-03-21 21:48:41 -0300

    updates

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import authRoutes from './AuthRoutes'
 import clientRoutes from './ClientRoutes'
+import productRoutes from './ProductRoute'
 import clientPaymentRoutes from './ClientPaymentRoutes'
 
 const router = express.Router()
 
 router.use("/auth", authRoutes)
 router.use("/clients", clientRoutes)
+router.use("/products", productRoutes)
 router.use("/clientPayments", clientPaymentRoutes)
 
 export default router
