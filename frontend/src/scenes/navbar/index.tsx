34f89ec72d7818e57f52d1d301543e7140e8aa92
Checho <ezequielayala.c@gmail.com>
2024-04-20 21:59:53 -0300

    progress

--- frontend/src/scenes/navbar/index.tsx
+++ frontend/src/scenes/navbar/index.tsx
@@ -1,5 +1,5 @@
 import { FlexBetween } from '@/components/FlexBetween'
-import { useTheme } from '@mui/material'
+import { Typography, useTheme } from '@mui/material'
 import {useState} from 'react'
 
 type Props = {}
@@ -7,8 +7,9 @@ type Props = {}
 const NavBar = (props: Props) => {
     const {palette} = useTheme()
   return (
-    <FlexBetween mb={'0.25rem'} p={'0.5rem'} color={palette.grey[100]} bgcolor={palette.secondary.dark}>
-        NAV
+    <FlexBetween mb={'0.25rem'} p={'0.5rem 1rem'} height={'4rem'} color={palette.grey[100]} bgcolor={palette.secondary.dark}>
+        <Typography variant='h3'>Customer Managment</Typography>
+        <Typography variant='h3'>User</Typography>
     </FlexBetween>
   )
 }
