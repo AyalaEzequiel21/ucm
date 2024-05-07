1888b97f8b479f1b0907959f9357d9a506c61334
Checho <ezequielayala.c@gmail.com>
2024-05-06 22:47:52 -0300

    updates

--- frontend/src/scenes/Clients/index.tsx
+++ frontend/src/scenes/Clients/index.tsx
@@ -1,71 +1,177 @@
-import { CustomDatGrid } from "@/components/CustomDataGrid"
-import { Header } from "@/components/Header"
-import { SceneContainer } from "@/components/SceneContainer"
-import useScreenSize from "@/hooks/useScreenSize"
-import { getSomeClients } from "@/utils/dataUtils/dataMock"
-import { getFormatedDate } from "@/utils/functionsHelper/getFormatedDate"
-import { getFormatedValue } from "@/utils/functionsHelper/getFormatedValue"
-import { IClient } from "@/utils/interfaces/IClient"
-import { Button } from "@mui/material"
-import { GridColDef } from "@mui/x-data-grid"
+import { CustomDatGrid } from "@/components/CustomDataGrid";
+import { Header } from "@/components/Header";
+import { SceneContainer } from "@/components/SceneContainer";
+import useScreenSize from "@/hooks/useScreenSize";
+import { getSomeClients } from "@/utils/dataUtils/dataMock";
+import { getFormatedDate } from "@/utils/functionsHelper/getFormatedDate";
+import { getFormatedValue } from "@/utils/functionsHelper/getFormatedValue";
+import { IClient } from "@/utils/interfaces/IClient";
+import { Button } from "@mui/material";
+import { GridColDef } from "@mui/x-data-grid";
 
-
-type ClientsProps = object
+type ClientsProps = object;
 
 const Clients: React.FC<ClientsProps> = () => {
+  const { isMobile, isTablet } = useScreenSize();
+  const clients = getSomeClients();
 
-    const {isMobile} = useScreenSize()
-    const clients = getSomeClients()
+  const handleDetailsClick = (client: IClient) => {
+    console.log(client);
+  };
 
-    const handleDetailsClick = (client: IClient) => {
-        console.log(client)
-    }
-
-    const columns: GridColDef<IClient>[] = [
-        { field: 'fullname', headerName: 'Cliente', flex: 1 },
-        { field: 'category', headerName: 'Categoria', hideable: !isMobile, flex: 0.5, renderCell(value) {
-          if(value.row.category === 'cat_1'){
-            return 'Cargador'
-          } else {
-            return 'Carnicero'
-          }
-        }},
-        { field: 'balance', headerName: 'Balance', flex: 0.5, renderCell(value) {
-            return getFormatedValue(value.row.balance)
-        }},
-        { field: 'in_delivery', headerName: 'Reparto', hideable: !isMobile, flex: 0.5, renderCell(value) {
-          if(value.row.in_delivery) {
-            return 'Si'
-          } else {
-            return 'No'
-          }
-        }},
-        { field: 'createdAt', headerName: 'Registro', hideable: !isMobile, flex: 0.5, renderCell(value){
-          return getFormatedDate(value.row.createdAt)
-        }},
-        {
-        field: 'details',
-        headerName: 'Details',
-        flex: 0.5,
-        renderCell: (params) => (
-            <Button key={params.row._id} onClick={() => handleDetailsClick(params.row)}>
-            Details
+  const columnsPitcher = (isMobile: boolean, isTablet: boolean): GridColDef<IClient>[] => {
+    if (isMobile) {
+      return [
+        { field: "fullname", headerName: "Cliente", flex: 1, renderCell(value){
+          return (
+            <Button
+              key={value.row._id}
+              onClick={() => handleDetailsClick(value.row)}
+            >
+              {value.row.fullname}
             </Button>
-        ),
+          )
+        } },
+        {
+          field: "balance",
+          headerName: "Balance",
+          flex: 0.5,
+          renderCell(value) {
+            return getFormatedValue(value.row.balance);
+          },
         },
-    ]
+      ];
+    } if(isTablet) {
+        return [
+          { field: "fullname", headerName: "Cliente", flex: 0.5 },
+          {
+            field: "category",
+            headerName: "Categoria",
 
-    return (
-            <SceneContainer>
-                <Header title="CLIENTES" subtitle="Lista de clientes" />
-                <CustomDatGrid<IClient> 
-                  rows={clients}
-                  columns={columns}
-                  isFilterName={true}
-                  fieldValue="fullname"
-                />
-            </SceneContainer>
-    )
-}
+            flex: 0.5,
+            renderCell(value) {
+              if (value.row.category === "cat_1") {
+                return "Cargador";
+              } else {
+                return "Carnicero";
+              }
+            },
+          },
+          {
+            field: "balance",
+            headerName: "Balance",
+            flex: 0.5,
+            renderCell(value) {
+              return getFormatedValue(value.row.balance);
+            },
+          },
+          {
+            field: "in_delivery",
+            headerName: "Reparto",
+            flex: 0.5,
+            renderCell(value) {
+              if (value.row.in_delivery) {
+                return "Si";
+              } else {
+                return "No";
+              }
+            },
+          },
+          {
+            field: "createdAt",
+            headerName: "Registro",
+            flex: 0.5,
+            renderCell(value) {
+              return getFormatedDate(value.row.createdAt);
+            },
+          },
+          {
+            field: "details",
+            headerName: "Details",
+            flex: 0.5,
+            renderCell: (params) => (
+              <Button
+                key={params.row._id}
+                onClick={() => handleDetailsClick(params.row)}
+              >
+                Details
+              </Button>
+            ),
+          },
+        ];
+    } else {
+      return [
+        { field: "fullname", headerName: "Cliente", flex: 1 },
+        {
+          field: "category",
+          headerName: "Categoria",
+          flex: 0.5,
+          renderCell(value) {
+            if (value.row.category === "cat_1") {
+              return "Cargador";
+            } else {
+              return "Carnicero";
+            }
+          },
+        },
+        {
+          field: "balance",
+          headerName: "Balance",
+          flex: 0.5,
+          renderCell(value) {
+            return getFormatedValue(value.row.balance);
+          },
+        },
+        {
+          field: "in_delivery",
+          headerName: "Reparto",
+          flex: 0.5,
+          renderCell(value) {
+            if (value.row.in_delivery) {
+              return "Si";
+            } else {
+              return "No";
+            }
+          },
+        },
+        {
+          field: "createdAt",
+          headerName: "Registro",
+          flex: 0.5,
+          renderCell(value) {
+            return getFormatedDate(value.row.createdAt);
+          },
+        },
+        {
+          field: "details",
+          headerName: "Details",
+          flex: 0.5,
+          renderCell: (params) => (
+            <Button
+              key={params.row._id}
+              onClick={() => handleDetailsClick(params.row)}
+            >
+              Details
+            </Button>
+          ),
+        },
+      ];
+    }
+  };
 
-export { Clients }
+  
+
+  return (
+    <SceneContainer>
+      <Header title="CLIENTES" subtitle="Lista de clientes" />
+      <CustomDatGrid<IClient>
+        rows={clients}
+        columns={columnsPitcher(isMobile, isTablet)}
+        isFilterName={true}
+        fieldValue="fullname"
+      />
+    </SceneContainer>
+  );
+};
+
+export { Clients };
